5caefcd693af3a78cc000ef1e47deddb5bf2e358
Aidar <Agsafiullin@gmail.com>
2021-02-07 20:22:34 +0300

    added project

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -11,9 +11,32 @@ class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
 
-        int actual = service.remain(amount);
+        int actual = service.remain(  amount);
         int expected = 100;
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldAmountMore1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = service.remain(  amount);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(  amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
 }
